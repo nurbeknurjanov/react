3c08971fbe022174dd6a3bace4f09b4b4f8be736
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2021-06-21 22:02:14 +0600

    tranlsations

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -3,18 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import {List, ListItem, ListItemText, Collapse} from '@material-ui/core';
 import {ExpandLess, ExpandMore} from '@material-ui/icons';
 import Link from 'components/standard/link';
+import {withNamespaces} from "react-i18next";
 
 
 
 
-const links = [
-    {label:'Home', pathname:'/' },
-    {label:'Users', pathname:'/users',
-        items: [
-            {label:'Create User', pathname:'/users/create'},
-        ],
-    },
-]
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,7 +20,17 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const Sidebar = ()=> {
+let Sidebar = ({t})=> {
+
+    const links = [
+        {label:t('home'), pathname:'/' },
+        {label:t('users'), pathname:'/users',
+            items: [
+                {label:t('user.create'), pathname:'/users/create'},
+            ],
+        },
+    ]
+
 
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
@@ -76,4 +79,5 @@ const Sidebar = ()=> {
     );
 }
 
+Sidebar = withNamespaces()(Sidebar);
 export default Sidebar;
