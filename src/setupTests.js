599acb67f0f5188200297f2e991676f605e74b7f
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2021-06-25 17:39:29 +0600

    tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,21 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import {unmountComponentAtNode} from "react-dom";
+
+//console.log('some tests')
+let container = null;
+beforeEach(() => {
+    // подготавливаем DOM-элемент, куда будем рендерить
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // подчищаем после завершения
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+export {container}
---
+++ src/tests/hello.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "react-dom";
+import { act } from "react-dom/test-utils";
+import {container} from '../setupTests';
+
+import Hello from "./hello";
+
+it("renders as guest", () => {
+    act(() => {
+        render(<Hello />, container);
+    });
+    expect(container.textContent).toBe("Hey, stranger");
+});
+it("renders as user", () => {
+    act(() => {
+        render(<Hello name="Jenny" />, container);
+    });
+    expect(container.textContent).toBe("Hello, Jenny!");
+
+    act(() => {
+        render(<Hello name="Margaret" />, container);
+    });
+    expect(container.textContent).toBe("Hello, Margaret!");
+});
